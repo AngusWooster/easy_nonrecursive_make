d432917f73f11fd19ec54013924730dc4235131c
angus <angus.ku@unex.com.tw>
2021-06-14 21:40:31 +0800

    feat(build): add ignored files

--- examples.mk
+++ examples.mk
@@ -1,17 +1,3 @@
-#export CC CF_ALL DIR_REL
-
-#EXAMPLE_FOLDER:= $(DIR_REL)/example
-#EXAMPLES: sub_compute
-
-
-
-#sub_compute:
-#	@#echo  DIR_REL = $(DIR_REL)
-#	@#echo  CC = $(CC)
-#	@#echo  CF_ALL = $(CF_ALL)
-#	@#echo  EXAMPLE_FOLDER = $(EXAMPLE_FOLDER)
-#	$(MAKE) -C $(EXAMPLE_FOLDER)/$@
-#
 
 $(target_examples):
 	@for ex in $(target_examples); \
